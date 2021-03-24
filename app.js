cfa8891d3f572457a0875e4797ec9608f1f093a6
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-03-24 12:32:12 -0700

    added answer validation to required questions

--- app.js
+++ app.js
@@ -18,12 +18,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'What is your GitHub username?'
+            message: 'What is your GitHub username?',
+            validate: username => {
+                if (username) {
+                    return true;
+                } else {
+                    console.log('Please enter your username');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -48,12 +64,28 @@ const promptProject = (portfolioData) => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the name of your project');
+                return false;
+            }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: description => {
+            if (description) {
+                return true;
+            } else {
+                console.log('Please enter a description');
+                return false;
+            }
+        }
       },
       {
         type: 'checkbox',
@@ -64,7 +96,15 @@ const promptProject = (portfolioData) => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: link => {
+            if (link) {
+                return true;
+            } else {
+                console.log('Please enter the link to your project');
+                return false;
+            }
+        }
       },
       {
         type: 'confirm',
